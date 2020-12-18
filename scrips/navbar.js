1de9794ef5e11ca2aab03b20085d7e925f39a4dd
donissousa <donisgtr@gmail.com>
2020-12-18 20:57:20 -0300

    nav bar commit

--- scrips/navbar.js
+++ scrips/navbar.js
@@ -3,10 +3,11 @@ const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
 const footer = document.querySelector("footer")
 const main = document.querySelector("main")
+const inicio = document.getElementById("inicio");
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle("open");
     footer.classList.toggle("visibility");
     main.classList.toggle("visibility");
-
+    inicio.remove();
 })
